889cb3c2f3a2f097de11a06e7bbe0b10bca22ec4
Jorge Valdivia <jorge@valdivia.me>
2012-11-18 13:21:08 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ballroom::Application.config.secret_token = '06f44d4cddd0dacc93b709ad6ef4a6e5f02250359257ef9ebcfa43aa835731eb913c968fc10cdfe0f248b6b4876221b53f0c0d48dea801374217a43cc4cc79cf'
